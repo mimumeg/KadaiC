fc9267c59fb4b33803cfea566f4654f61930ba44
mimumeg <meg.stella555@gmail.com>
2018-11-22 21:24:20 +0900

    C-2完成 『アーセナル』というキーワードがあればフィルタをかけて<censored>を返すことができた。replace()が使える！

--- word_filter.py
+++ word_filter.py
@@ -11,6 +11,8 @@ class WordFilter:
         # else:
         #     return False
 
+    def censor(self, text):
+        print(text.replace("アーセナル", "<censored>"))
 
 if __name__ == "__main__":
     my_filter = WordFilter("アーセナル")  # インスタンス化
@@ -20,4 +22,10 @@ if __name__ == "__main__":
     my_filter.detect("昨日のアーセナルの試合アツかった！")  # Trueを返す
 
     # NGワードが含まれていない場合
-    my_filter.detect("昨日のリバプールの試合アツかった！")  # Falseを返す ※出力されるわけではありません！
+    my_filter.detect("昨日のリバプールの試合アツかった！")  # Falseを返す
+
+    # NGワードが含まれている場合
+    my_filter.censor("昨日のアーセナルの試合アツかった！")  # "昨日の<censored>の試合アツかった！" を返す
+
+    # NGワードが含まれていない場合
+    my_filter.censor("昨日のリバプールの試合アツかった！")  # "昨日のリバプールの試合アツかった！" を返す
